8b5f0099cdc5da31c5ae4f853f09c9e5c51757f7
falsekin <135793850+falsekin@users.noreply.github.com>
2023-06-07 16:02:35 +0900

    Update test.js

--- test.js
+++ test.js
@@ -15,3 +15,13 @@ function getMDToken(successCallbackJS, failCallbackJS){
         alert('test552');
     }
 getMDToken();
+
+function callback(v){
+	var actk=v.accessToken;
+	var rftk=v.refreshToken;
+	alert(actk);
+	alert(rftk);
+	alert('test');
+}
+window.wallet["getMDToken"]('{"successCallbackJS":"callback", "failCallbackJS":""}');
+callback();
