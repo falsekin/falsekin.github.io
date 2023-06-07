79f2b1d8fd2c6f53097a866c99da9648e4d6a607
falsekin <135793850+falsekin@users.noreply.github.com>
2023-06-07 16:13:07 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,3 @@
-function getMDToken(successCallbackJS, failCallbackJS){
-        alert('test1');
-        var func = window[successCallbackJS] || eval(successCallbackJS);
-        if (!window.wallet || !window.wallet.getMDToken) return false;
-        try {
-            window.wallet.getMDToken(JSON.stringify({
-                "successCallbackJS": successCallbackJS,
-                "failCallbackJS": failCallbackJS
-            }));
-        }catch (e){
-        }
-        alert(JSON.stringify(successCallbackJS));
-        alert('test552');
-    }
-//getMDToken();
-
 function callback(v){
 	var actk=v.accessToken;
 	var rftk=v.refreshToken;
